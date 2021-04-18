36df22e086117ec3b7018c895c59049a8b5a39f4
George Jacob <georgekunchattil@gmail.com>
2021-04-18 09:57:40 +0530

    index web page created

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Image Abstractor</title>
+   <link rel="stylesheet" type="text/css" href="style.css" media="all">
+ </head>
+
+ <body>
+
+ 	<header class="masthead">
+ 		<div class="site-branding">
+ 			<h1 class="site-title">Image Abstractor</h1>
+ 			<p class="site-description">Pull the most domin</p>
+ 		</div>
+ 	<header>
+ 	<section>
+ 		<p>test</p>
+ 	</section>
+ </body>
+</html>
